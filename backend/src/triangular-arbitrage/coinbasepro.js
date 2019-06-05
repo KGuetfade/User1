4e79bf00e6564963d3c3aa4fec962c485b759ccc
Karim Guettache <karim.guettache@gmail.com>
2019-06-05 13:51:04 +0200

    Changed calculate method

--- backend/src/triangular-arbitrage/coinbasepro.js
+++ backend/src/triangular-arbitrage/coinbasepro.js
@@ -39,10 +39,9 @@ class CoinbaseProTriArbScanner {
             const final = []
             for (let i = 0; i < this.arbitrage_pairs.length; i++) {
                 const pairs = this.arbitrage_pairs[i]
-                const data = await this.getTickers(pairs)
-                final.push(data)
-                /* const arbitrage = this.calculateArbitrage(data)
-                final.push(arbitrage) */
+                const data = await this.getOrderbook(pairs)
+                const arbitrage = this.calculateArbitrage(data)
+                final.push(arbitrage)
                 await timeout(500)
             }
             return final 
@@ -53,6 +52,18 @@ class CoinbaseProTriArbScanner {
         const original = this.client.getProductTicker(arbitragePairs.original)
         const base = this.client.getProductTicker(arbitragePairs.base)
         const quote = this.client.getProductTicker(arbitragePairs.quote)
+        const result = await Promise.all([original, base, quote])
+        return { 
+            original: {id: arbitragePairs.original, ...result[0]}, 
+            base: {id: arbitragePairs.base, ...result[1]}, 
+            quote: {id: arbitragePairs.quote, ...result[2]} 
+        }
+    }
+
+    async getOrderbook(arbitragePairs, level=1) {
+        const original = this.client.getProductOrderBook(arbitragePairs.original, { level })
+        const base = this.client.getProductOrderBook(arbitragePairs.base, { level })
+        const quote = this.client.getProductOrderBook(arbitragePairs.quote, { level })
         const result = await Promise.all([original, base, quote])
         return { 
             original: {id: arbitragePairs.original, ...result[0]}, 
@@ -79,14 +90,14 @@ class CoinbaseProTriArbScanner {
             const other = Object.assign({}, allPairs[i])
             if (other.id === exclude_id) { continue }
             if (currency === other.base_currency) { 
-                this.reverseCurrency(other)
+                this.reverseQuoteBase(other)
                 markets.push(other) 
             } else if (currency === other.quote_currency) { markets.push(other) }
         }
         return markets
     }
 
-    reverseCurrency(currency) {
+    reverseQuoteBase(currency) {
         const temp_quote = currency.quote_currency
         currency.quote_currency = currency.base_currency
         currency.base_currency = temp_quote
@@ -105,8 +116,8 @@ class CoinbaseProTriArbScanner {
 
                         const copy_base = Object.assign({}, base)
                         const copy_quote = Object.assign({}, quote)
-                        if (copy_base.switched) { this.reverseCurrency(copy_base) }
-                        if (copy_quote.switched) { this.reverseCurrency(copy_quote) }
+                        if (copy_base.switched) { this.reverseQuoteBase(copy_base) }
+                        if (copy_quote.switched) { this.reverseQuoteBase(copy_quote) }
                         
                         acc.push({ 
                             original : original.id, 
@@ -141,49 +152,114 @@ class CoinbaseProTriArbScanner {
     calculateArbitrage(pair) {
         const { original, base, quote } = pair
 
-        const one = new Decimal(1)
-        const base_price = new Decimal(base.price)
-        const quote_price = new Decimal(quote.price)
-        const price = new Decimal(original.price)
+        const base_quote = this.getTriangleBaseQuote(pair)
+        const quote_base = this.getTriangleQuoteBase(pair)
 
-        const switched_base_price = base.switched ? one.dividedBy(base_price) : base_price
-        const switched_quote_price = quote.switched ? one.dividedBy(quote.price) : quote_price
-
-        const arbitragePrice = switched_quote_price.dividedBy(switched_base_price)
-
-        if (arbitragePrice >= price) {
-            //calculate arbitrage using bid and ask
-        } else {
-            //calculate arbitrage using bid and ask
+        return {
+            original,
+            base,
+            quote,
+            base_quote,
+            quote_base
         }
         
-        /* if (indicated_price >= price) {
-            const percentage = math.divide(math.bignumber(indicated_price), math.bignumber(price))
-            return { 
-                original: original.id, 
-                pair1: base.id, 
-                pair2: quote.id,                     
-                pair1_price: math.format(adjusted_base_price, { precision }),
-                pair2_price: math.format(adjusted_quote_price, { precision }),
-                original_price: math.format(price, { precision }),
-                triangular_price: math.format(indicated_price, { precision }),
-                percentage: math.format(percentage, { precision }),
-                time: new Date() 
+    }
+
+    getBase(id) {
+        return id.split('-')[0]
+    }
+
+    getQuote(id) {
+        return id.split('-')[1]
+    }
+
+    deepClone(arr) {
+        return [[...arr[0]]]
+    }
+
+    reverseBidAsk(currency) {
+        const base = this.getQuote(currency.id)
+        const quote = this.getBase(currency.id)
+        currency.id = base + '-' + quote
+
+        const temp_bids = this.deepClone(currency.bids)
+        currency.bids = this.deepClone(currency.asks)
+        currency.asks = temp_bids
+
+        currency.bids[0][1] = (new Decimal(currency.bids[0][0])).times(currency.bids[0][1]).toString()
+        currency.bids[0][0] = (new Decimal(1)).dividedBy(currency.bids[0][0]).toString()
+
+        currency.asks[0][1] = (new Decimal(currency.asks[0][0])).times(currency.asks[0][1]).toString()
+        currency.asks[0][0] = (new Decimal(1)).dividedBy(currency.asks[0][0]).toString()
+    }
+
+    shouldReverseBidAsk(original, currency) {
+        if (this.getQuote(currency.id) !== this.getBase(original.id)) {
+            if (this.getQuote(currency.id) !== this.getQuote(original.id)) {
+                return true
             }
-        } else {
-            const percentage = math.divide(math.bignumber(price), math.bignumber(indicated_price))            
-            return { 
-                original: original.id, 
-                pair1: base.id, 
-                pair2: quote.id,                     
-                pair1_price: math.format(adjusted_base_price, { precision }),
-                pair2_price: math.format(adjusted_quote_price, { precision }),
-                original_price: math.format(price, { precision }),
-                triangular_price: math.format(indicated_price, { precision }),
-                percentage: math.format(percentage, { precision }),
-                time: new Date()  
-            }
-        }  */
+        }
+        return false
+    }
+
+    getTriangleBaseQuote(pair, size=false) {
+        const original = Object.assign({}, pair.original)
+        const base = Object.assign({}, pair.base)
+        const quote = Object.assign({}, pair.quote)
+        
+        if (this.shouldReverseBidAsk(original, base)) { this.reverseBidAsk(base) }
+        if (this.shouldReverseBidAsk(original, quote)) { this.reverseBidAsk(quote) }
+
+        const start = new Decimal('1')
+        const trade1 = start.times(original.bids[0][0])
+        const trade2 = trade1.dividedBy(quote.asks[0][0])
+        const trade3 = trade2.times(base.bids[0][0])
+            
+        const percentage = trade3.dividedBy(start).minus(1).times(100)
+        return percentage
+    }
+
+    getTriangleQuoteBase(pair, size=false) {
+        const original = Object.assign({}, pair.original)
+        const base = Object.assign({}, pair.base)
+        const quote = Object.assign({}, pair.quote)
+        
+        if (this.shouldReverseBidAsk(original, base)) { this.reverseBidAsk(base) }
+        if (this.shouldReverseBidAsk(original, quote)) { this.reverseBidAsk(quote) }
+
+        const start = new Decimal('1')
+        const trade1 = start.dividedBy(original.asks[0][0])
+        const trade2 = trade1.dividedBy(base.asks[0][0])
+        const trade3 = trade2.times(quote.bids[0][0])
+        
+        const percentage = trade3.dividedBy(start).minus(1).times(100)
+        return percentage
+    }
+
+    maxSizeBaseQuote(original, base, quote) {
+        let smallest_amount = original.size
+
+        const amount_orignal = (new Decimal(original.price)).times(smallest_amount)
+        let amount_quote = (new Decimal(amount_orignal)).dividedBy(quote.price)
+
+        if (amount_quote >= quote.size) {
+            amount_quote = (new Decimal(quote.price)).times(quote.size)
+            smallest_amount = amount_quote.dividedBy(original.price)
+            
+        }
+
+        let amount_base = amount_quote.times(base.price)
+        if (amount_base >=  base.size) {
+            amount_base = (new Decimal(base.size)).dividedBy(base.price)
+            amount_quote = amount_base.times(quote.price)
+            smallest_amount = amount_quote.dividedBy(original.price)
+        }
+
+        return smallest_amount
+    }
+
+    maxSizeQuoteBase(pair) {
+
     }
 
 }
