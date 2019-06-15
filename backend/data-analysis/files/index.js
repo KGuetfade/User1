6b8f7a40ecd79be829b95206887e1b43ae2925a9
Karim Guettache <karim.guettache@gmail.com>
2019-06-15 15:02:28 +0200

    Fixed data visualization

--- backend/data-analysis/files/index.js
+++ backend/data-analysis/files/index.js
@@ -1,5 +1,4 @@
 const btcprice = 7503.77
-const minimumVolume = 15
 const fees = [.75, .6, .54, .45, .3, .24, .21, .18, .15]
 const volumeIntervals = [100, 200, 500, 750, 1000, 2000, 5000, 10000, 20000]
 
@@ -18,7 +17,10 @@ fetch('http://localhost:3000/data')
         
         return JSON.parse(data)
     })
-    .then(data => createCharts(data))
+    .then(data => {
+        $('#spinner').css('display', 'none')
+        createCharts(data)
+    })
     .catch(err => console.log(err))
 
 const createCharts = data => {
@@ -29,7 +31,7 @@ const createCharts = data => {
             labels: fees,
             datasets: [{
                 label: 'profit per fee tier',
-                data: fees.map(f => getProfitAbovePercentage(f, data, minimumVolume))
+                data: fees.map(f => getProfitAbovePercentage(f, data))
             }]
         }
     })
@@ -46,17 +48,29 @@ const createCharts = data => {
         }
     })
 
+    const profitVolumeGraph = $('#profitVolumeGraph')
+    const profitVolumeChart = new Chart(profitVolumeGraph, {
+        type: 'line',
+        data : {
+            labels: volumeIntervals,
+            datasets: [{
+                label: 'profit per volume',
+                data: volumeIntervals.map(v => ({x: v, y: getProfitPerVolume(fees[0], data, v)}))
+            }]            
+        }
+    })
+
     
 }
 
-const getProfitAbovePercentage = (percentage, data, vol) => {
+const getProfitAbovePercentage = (percentage, data) => {
     const above = data.filter(d => d.percentage >= percentage)
     const profit = above.reduce((acc, d) => {
         const step = d.steps[0]
         const euro = step.type === 'buy' ? step.funds : step.size * btcprice
         const net_percentage = d.percentage - percentage
         
-        if (euro <= vol) { return acc }
+        if (euro <= 15) { return acc }
 
         const profit = (net_percentage / 100) * euro
         return acc + profit
@@ -75,4 +89,19 @@ const getTotalVolumeAbovePercentage = (percentage, data) => {
         return acc + (euro * 3)
     }, 0)
     return volume
+}
+
+const getProfitPerVolume = (percentage, data, volume) => {
+    const above = data.filter(d => d.percentage >= percentage)
+    const profit = above.reduce((acc, d) => {
+        const step = d.steps[0]
+        const euro = step.type === 'buy' ? step.funds : step.size * btcprice
+        const net_percentage = d.percentage - percentage
+        
+        if (euro > 15 && euro <= volume) {
+            const profit = (net_percentage / 100) * euro
+            return acc + profit
+        } else { return acc }        
+    }, 0)
+    return profit
 }
